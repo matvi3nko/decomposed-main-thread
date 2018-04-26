e84f73b9622f84e91a51040276101352dc2f891c
Nikolay Matvienko <v-matvn-gri@ae.com>
2018-04-26 14:31:55 +0300

    express server converted to createExpressServer factory

--- express.js
+++ express.js
@@ -3,51 +3,41 @@
 const nr = require('newrelic');
 const express = require('express');
 const app = express();
-const CpuTask = require('./processPool/cpuTask');
 
-//Logger
-const winston = require('winston');
-const Elasticsearch = require('winston-elasticsearch');
-const { routes, port, data, sum } = require('./config');
+module.exports = function createServer (config, logger, CpuTask) {
+    const { routes, port, data, sum } = config;
 
-const esTransportOpts = {
-    level: 'info'
-};
-const logger = new winston.createLogger({
-    transports: [
-        new Elasticsearch(esTransportOpts)
-    ]
-});
-
-routes.forEach(route => {
-    app.get(`/${route}`, function (req, res) {
-        res.send({greeting: 'Hello HolyJS!'});
-    });
-});
-
-app.get('/cpu', function (req, res) {
-    const subsetSum = new CpuTask(sum, data);
-
-    return subsetSum.start()
-        .then(result => {
-            const meta = {
-                method: req.method,
-                url: req.url,
-                headers: req.headers
-            };
-
-            logger.info(meta, 'Winston log');
-
-            return res.send(result);
-        })
-        .catch(error => {
-            console.error(error);
-            res.send(error);
+    routes.forEach(route => {
+        app.get(`/${route}`, function (req, res) {
+            res.send({ greeting: 'Hello HolyJS!' });
         });
-});
+    });
 
+    app.get('/cpu', function (req, res) {
+        const subsetSum = new CpuTask(sum, data);
 
-app.listen(port, function () {
-    console.log(`Server process: ${process.pid}`);
-    console.log(`Express server listening on port ${port}!`);
-});
+        return subsetSum.start()
+            .then(result => {
+                const meta = {
+                    method: req.method,
+                    url: req.url,
+                    headers: req.headers
+                };
+
+                logger.info(meta, 'Winston log');
+
+                return res.send(result);
+            })
+            .catch(error => {
+                console.error(error);
+                res.send(error);
+            });
+    });
+
+    app.listen(port, function () {
+        console.log(`Server process: ${process.pid}`);
+        console.log(`Express server listening on port ${port}!`);
+    });
+
+    return app;
+};
