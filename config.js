08bdb6a5065af201ceecc5034007d27f5f876eec
Nikolay Matvienko <v-matvn-gri@ae.com>
2018-04-26 14:02:27 +0300

    Express web server with Logger, New Relic and Cpu-bound Task

---
+++ config.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Nikolay Matvienko on 4/25/18.
+ */
+
+module.exports = {
+    graphiteHost : 'plaintext://cc5d5e19.carbon.hostedgraphite.com:2003/',
+    routes : ['', 'a', 'aa', 'b', 'bb', 'c', 'cc', 'cca', 'ccb', 'cp', 'cpa', 'cpb', 'cpc'],
+    port: 3000,
+    data: [100, -1, -142, 43, -58, 184, 0, 42],
+    sum: 42
+};
